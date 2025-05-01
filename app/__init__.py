2f4653449d871d39ddc7af083d027fc8bc1dd151
emirhandumann <emirdumanhan@gmail.com>
2025-05-01 19:40:38 +0300

    about and contact pages created

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
 from config import Config
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
 
-    @app.route('/')
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    @app.route("/")
     def index():
-        return render_template('index.html')
+        return render_template("index.html")
 
-    return app 
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
+
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
+
+    return app
