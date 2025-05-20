a95c3b36c2140f4be1fd5c5d3fdd1bc2254151b7
emirhandumann <emirdumanhan@gmail.com>
2025-05-20 22:11:59 +0300

    hotfix

--- test_load_cell.py
+++ test_load_cell.py
@@ -4,7 +4,8 @@ from hx711 import HX711
 GPIO.setmode(GPIO.BCM)
 
 hx = HX711(dout_pin=6, pd_sck_pin=5)
+hx.zero()
 
 while True:
-    reading = hx.get_raw_data_mean()
+    reading = hx.get_data_mean()
     print(reading)
